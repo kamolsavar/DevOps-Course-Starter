baa93faa057bb5daca2b6e4b76517bbc4fcec4ac
kamolsavar <kamolsavar@gmail.com>
2021-04-08 13:53:59 +0100

    app file has been changed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask,  render_template
+from todo_app.data import session_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,8 +8,15 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
+@app.route('/addList')
+def addList():
+    return render_template('indexList.html')
+
+@app.route('/addTitles')
+def addTitle():
+    return session_items.add_item(title)
 
 if __name__ == '__main__':
     app.run()
