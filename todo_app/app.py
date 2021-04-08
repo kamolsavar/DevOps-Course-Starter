468b14dc83f3b3316ad25c2dbb38eba08d22777b
kamolsavar <kamolsavar@gmail.com>
2021-04-08 19:44:58 +0100

    html file removed

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,21 +2,21 @@ from flask import Flask,  render_template
 from todo_app.data import session_items
 from todo_app.flask_config import Config
 
+list = ['Harry Potter', 'Sherlock Holmes', 'Dino Master']
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/addList')
-def addList():
-    return render_template('indexList.html')
+    return render_template('index.html', list = list)
 
 @app.route('/addTitles')
 def addTitle():
-    return session_items.add_item(title)
+    return session_items.add_item('Never Land')
+def getTitle():
+    return session_items.get_item('4')         
 
 if __name__ == '__main__':
     app.run()
