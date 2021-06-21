93185830ed9e87373e2e36eb2b5f6a0df625abbd
kamolsavar <kamolsavar@gmail.com>
2021-06-21 17:51:13 +0100

    other unit tests are refactored

--- todo_app/test/test_unit.py
+++ todo_app/test/test_unit.py
@@ -11,11 +11,11 @@ def test_get_ToDo_Trello():
     assert len(list)==1
 
 def test_get_Doing_Trello():
-    viewModel = ViewModel(items=[{"idList": "60af9248e87283184d346aaa", "id": "60bf89d905749b885a39b6ec","name":"Doing"}])
+    viewModel = ViewModel(items=[Item("60bf89d905749b885a39b6ec","title","Doing")])
     list= viewModel.doing_items
     assert len(list)==1
     
 def test_get_Done_Trello():
-    viewModel = ViewModel(items=[{"idList": "60af9248e87283184d346aab", "id": "60b7fe3a74f9c35594741e71","name":"Done"}])
+    viewModel = ViewModel(items=[Item("60b7fe3a74f9c35594741e71","title","Done")])
     list= viewModel.done_items
     assert len(list)==1
