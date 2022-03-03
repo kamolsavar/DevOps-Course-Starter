986bfa07cda674527065a97208deb4e796a49dd4
kamolsavar <kamolsavar@gmail.com>
2022-03-03 18:01:18 +0000

    config uncommented

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -5,5 +5,5 @@ class Config:
 
     def __init__(self):
         self.SECRET_KEY = os.environ.get('SECRET_KEY')
-        # if not self.SECRET_KEY:
-        #     raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
+        if not self.SECRET_KEY:
+            raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
