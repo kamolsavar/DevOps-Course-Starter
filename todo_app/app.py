dc29fba244b039940d596bdca80a9d3431f2652d
kamolsavar <kamolsavar@gmail.com>
2022-03-25 17:36:18 +0000

    authenticatio is completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,3 +1,4 @@
+from email.headerregistry import ContentTypeHeader
 import os
 import certifi
 from bson import ObjectId
@@ -9,35 +10,40 @@ from todo_app.flask_config import Config
 from flask import request
 import requests
 from todo_app.view_model import ViewModel 
-from flask_login import login_user, logout_user, LoginManager, login_required
-from oauthlib.oauth2 import WebApplicationClient    
+from flask_login import login_user, logout_user, LoginManager, login_required, UserMixin
+from oauthlib.oauth2 import WebApplicationClient  
+from todo_app.user import  User
 
 def create_app():
    app = Flask(__name__)
-   app.config.from_object(Config()) 
+   app.config.from_object(Config())
+   app.config['LOGIN_DISABLED'] = os.getenv('LOGIN_DISABLED') == 'True'
    client = pymongo.MongoClient(os.getenv("MONGO_DB_CONNECTION"), tlsCAFile=certifi.where())
    db = client[os.getenv("DATABASE_NAME")] 
    collection = db.test_collection
 
    login_manager = LoginManager()
-
+   
    @login_manager.unauthorized_handler
    def unauthenticated():
       return redirect('https://github.com/login/oauth/authorize?client_id=' + os.getenv("CLIENT_ID"))  
    
    @login_manager.user_loader
    def load_user(user_id):
-      return None
+      return User(user_id)
  
    login_manager.init_app(app)
 
    @app.route('/login/callback')
    def log_in_call_back():
-      response = requests.post('https://github.com/login/oauth/access_token', params={"client_id":os.getenv("CLIENT_ID"), "client_secret":os.getenv("CLIENT_SECRET"), "code": request.args.get("code")})
-      return response.text
-
-   def use_token_call_api():
-      requests.get('https://api.github.com/user',params={""})
+      response = requests.post('https://github.com/login/oauth/access_token', headers = {"Accept":"application/json"}, params={"client_id":os.getenv("CLIENT_ID"), "client_secret":os.getenv("CLIENT_SECRET"), "code": request.args.get("code")})
+      access_token = response.json()["access_token"]
+      response_user_data = requests.get('https://api.github.com/user', headers = {"Authorization" : f"Bearer {access_token}"}) 
+      user_id =  response_user_data.json()["id"]
+      user = User(user_id)
+      login_user(user)
+      return redirect(url_for('index')) 
+  
 
    @app.route('/')
    @login_required
@@ -52,8 +58,6 @@ def create_app():
       print (f"The record is {json_record_for_todos}" )
       view_model = ViewModel(list)
       return  render_template('index.html', view_model=view_model)
-
-      
 
    @app.route('/addNewTitle',methods = ['POST'])
    def addTitle():
