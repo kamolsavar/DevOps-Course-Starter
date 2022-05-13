47052ff0ac0d7e3b1b574ac8c4be5dea1cac6aa1
Kamol Saha <kamolsaha@Kamols-MacBook-Air.local>
2022-05-13 18:33:26 +0100

    service plan configured

--- main.tf
+++ main.tf
@@ -14,3 +14,28 @@ provider "azurerm" {
 data "azurerm_resource_group" "main" {
   name = "AmericanExpress21Group1_KamolSaha_ProjectExercise"
 }
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "terraformed-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Basic"
+    size = "B1"
+} }
+resource "azurerm_app_service" "main" {
+  name                = "<APP_NAME>"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.main.id
+
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "DOCKER|appsvcsample/python-helloworld:latest"
+  }
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+  } 
+}
