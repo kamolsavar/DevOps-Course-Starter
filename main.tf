43c2aea2185fbefcdf53a85294230a0333fc0e88
Kamol Saha <kamolsaha@Kamols-MacBook-Air.local>
2022-05-16 16:22:04 +0100

    mongo cosmo db has been added

--- main.tf
+++ main.tf
@@ -38,7 +38,83 @@ resource "azurerm_app_service" "main" {
     app_command_line = ""
     linux_fx_version = "DOCKER|appsvcsample/python-helloworld:latest"
   }
+
   app_settings = {
     "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
-  } 
+    "CLIENT_ID" = 
+    "CLIENT_SECRET" =
+    "DATABASE_NAME" = 
+    "DOCKER_ENABLE_CI" = 
+    "DOCKER_REGISTRY_SERVER_URL" =
+    "MONGO_DB_CONNECTION" = azurerm_cosmosdb_account.main.connection_strings[0]
+    "SECRET_KEY" = 
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = false
+  }
+
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "tfex-cosmos-db-${random_integer.ri.result}"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities { 
+    name = "EnableServerless" 
+  }
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  lifecycle { 
+    prevent_destroy = true 
+    }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = var.failover_location
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = data.azurerm_resource_group.main.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "myfirstTerraformDb" {
+  name                = "tfex-cosmos-mongo-db"
+  resource_group_name = azurerm_cosmosdb_account.db.resource_group_name
+  account_name        = azurerm_cosmosdb_account.db.name
 }
---
+++ variable.tf
@@ -0,0 +1,9 @@
+
+variable "prefix" {
+  description = "The prefix used for all resources in this environment"
+}
+
+variable "location" {
+  description = "The Azure location where all resources in this deployment should be created"
+  default     = "uksouth"
+}
