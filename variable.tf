955a1eb27fd736137555d1a510be3b41fc163191
Kamol Saha <kamolsaha@Kamols-MacBook-Air.local>
2022-05-24 16:48:24 +0100

    todo.yml file had been updated with env

--- variable.tf
+++ variable.tf
@@ -1,7 +1,4 @@
 
-variable "prefix" {
-  description = "The prefix used for all resources in this environment"
-}
 
 variable "location" {
   description = "The Azure location where all resources in this deployment should be created"
