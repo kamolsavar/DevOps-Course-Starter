9eefeb75b8897e44dbb3e2b0155bb0f3875b3e27
Kamol Saha <kamolsaha@Kamols-MacBook-Air.local>
2022-05-24 17:36:31 +0100

    output file has been updated

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "webapp_url" {
   value = "https://${azurerm_app_service.main.default_site_hostname}"
 }
 
-output "webhook_url" {
+output "cd_webhook" {
   value = "https://${azurerm_app_service.main.site_credential[0].username}:${azurerm_app_service.main.site_credential[0].password}@${azurerm_app_service.main.name}.scm.azurewebsites.net/docker/hook"
 
 sensitive = true
