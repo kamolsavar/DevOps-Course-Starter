cfac682e7fccd8f2999fc13c1e332b4aeeafa3eb
Kamol Saha <kamolsaha@Kamols-MacBook-Air.local>
2022-05-24 16:58:25 +0100

    docker image name has been corrected

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ terraform {
         storage_account_name = "kamol2022amex"
         container_name       = "terraform-state"
         key                  = "terraform.tfstate"
-        access_key           = "aalgXIUW2QXHM5ngXCMtW3pq2bQsi7zVdQxqIn73HffrQbA1siAg6gZwxYTNmz7eVHBU93w7MAwrz3MwfJs5lg=="
     }
 }
 
